fe39b62353466216022012c28369ff9253c781f0
alu0101128931 <72500073+alu0101128931@users.noreply.github.com>
2023-01-13 19:55:47 +0000

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
 import Link from 'next/link'
+import Head from "next/head";
+import styles from "./index.module.css";
 
-export default function Home() {
+function Home() {
   return (
-    <ul>
-      <li> 
-        <Link href='/pet'> Generador de nombres de mascotas </Link>
-      </li>
-      <li> 
-        <Link href='/image'> Generador imagenes </Link>
-      </li>
-    </ul>
+    <div>
+      <Head>
+        <title>Inicio</title>
+        <link rel="icon" href="/photo_icon.jpg" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/photo_icon.jpg" className={styles.icon} />
+        <h2>Inicio</h2>
+        <h3>Eliga un enlace</h3>
+        <Link href="/generate">Generador de nombres para la mascota</Link>
+        <br></br>
+        <Link href="/image">Generador de imágenes</Link>
+        <br></br>
+        <Link href="/404">Página de error</Link>
+      </main>
+    </div>
   )
 }
+export default Home
