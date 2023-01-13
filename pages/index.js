a1475027ac6e655884bdd58ca0f15cc49e90ed1e
alu0101128931 <72500073+alu0101128931@users.noreply.github.com>
2023-01-13 20:12:08 +0000

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,16 @@ import styles from "./index.module.css";
 function Home() {
   return (
     <div>
-      <Head>
-        <title>Inicio</title>
-        <link rel="icon" href="/cam-icon.png" />
-      </Head>
 
       <main className={styles.main}>
         <img src="/cam-icon.png" className={styles.icon} />
+        </main>
         <h2>Inicio</h2>
         <h3>Eliga un enlace</h3>
         <Link href="/pet">Generador de nombres para la mascota</Link>
         <br></br>
+        <main className={styles.main}>
+        <img src="/cam-icon.png" className={styles.icon} />
         <Link href="/image">Generador de imágenes</Link>
         <br></br>
         <Link href="/404">Página de error</Link>
